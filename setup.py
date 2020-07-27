d14561dcfd2fef274a381b5dca81501f77852d80
Manu Blanco Valentín <manuelblancovalentin@users.noreply.github.com>
2020-07-27 19:45:24 -0300

    Distro update with minor bugs fixed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='TensorPlot',
-    version='0.0.1',
+    version='0.0.2',
     packages=['tensorplot'],
     url='http://github.com/manuelblancovalentin/tensorplot',
     license='GPL',
