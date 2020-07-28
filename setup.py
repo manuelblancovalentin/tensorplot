06f2a3dfe3fb94afbce70e0a47e11ff5cda47155
Manu Blanco Valentín <manuelblancovalentin@users.noreply.github.com>
2020-07-27 23:01:24 -0300

    New version released

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='TensorPlot',
-    version='0.0.3',
+    version='0.0.4',
     packages=['tensorplot'],
     url='http://github.com/manuelblancovalentin/tensorplot',
     license='GPL',
